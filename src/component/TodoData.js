07cc30e14f6fe6d33d29b7b36d04a9df9318e675
victoriaoak <89366891+victoriaoak@users.noreply.github.com>
2022-07-16 22:51:25 -0400

    backend GET requests

--- src/component/TodoData.js
+++ src/component/TodoData.js
@@ -3,7 +3,22 @@ import Axios from "axios";
 
 
 const ShowTodos = () => {
-  //Begin Here
+  const [todos, setTodos] = useState([]);
+        
+  // wait for a call to a backend service to complete, then proceed to render
+  useEffect( () => { 
+      async function fetchData() {
+          try {
+              const res = await Axios.get('http://localhost:8080/get/items'); 
+              setTodos(JSON.stringify(res.data));
+              console.log(JSON.stringify(res.data));
+          } catch (err) {
+              console.log(err);
+          }
+      }
+      fetchData();
+  }, []);
+  return <div>{todos}</div>
   
 }
 export default ShowTodos;
